bb857db9b903c3e55d66217d392f517560849e74
IamBemooooo <nguyendinhthe405@gmail.com>
2024-11-01 09:43:36 +0700

    Update model

--- HoanBds/Models/Products/ProductSpecificationDetailMongoDbModel.cs
+++ HoanBds/Models/Products/ProductSpecificationDetailMongoDbModel.cs
@@ -1,13 +1,20 @@
-﻿namespace HoanBds.Models.Products
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace HoanBds.Models.Products
 {
     public class ProductSpecificationDetailMongoDbModel
     {
-       
+        [BsonElement("_id")]
         public string _id { get; set; }
-  
+        public void GenID()
+        {
+            _id = ObjectId.GenerateNewId().ToString();
+        }
         public int attribute_id { get; set; }
         public int value_type { get; set; }
         public string value { get; set; }
+        public string type_ids { get; set; }
 
     }
 }
